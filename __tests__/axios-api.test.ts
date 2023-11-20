d608af820d79072aedad48c3876344ea9e682910
Tsatsu Adogla-Bessa Jnr <tsatsujnr@gmail.com>
2023-11-20 14:41:32 +0000

    implement headers interface

--- __tests__/axios-api.test.ts
+++ __tests__/axios-api.test.ts
@@ -72,7 +72,7 @@ describe("Axios API Client Retry Logic", () => {
   it("should be able to retry a request", async () => {
     nock(TEST_URL).get("/posts/1").times(4).reply(502)
 
-    const client = new AxiosApiClient(TEST_URL, undefined, undefined, undefined, (retryCount) => {
+    const client = new AxiosApiClient(TEST_URL, undefined, undefined, undefined, undefined, (retryCount) => {
       console.log(retryCount)
     })
     const spy = jest.spyOn(console, "log")
@@ -83,5 +83,118 @@ describe("Axios API Client Retry Logic", () => {
       expect(spy).toHaveBeenCalledWith(2)
       expect(spy).toHaveBeenCalledWith(3)
     }).rejects.toThrow(new AxiosError("Request failed with status code 502"))
+  })
+})
+
+describe("Axios API Client Headers Interface", () => {
+  it("should set headers correctly when initialized with axios instance", async () => {
+    const client = new AxiosApiClient(TEST_URL, {
+      "Content-Type": "application/json",
+      Authorization: "Bearer token",
+    })
+
+    nock(TEST_URL, {
+      reqheaders: {
+        "Content-Type": (headerValue) => headerValue === "application/json",
+        Authorization: (headerValue) => headerValue === "Bearer token",
+      },
+    })
+      .get("/posts/1")
+      .reply(200, { id: 1 })
+
+    const result = await client.get("/posts/1")
+    expect(result.status).toBe(200)
+    expect(result.data).toBeDefined()
+    expect(result.data).toHaveProperty("id")
+  })
+
+  it("should set headers correctly when configured at request level", async () => {
+    const client = new AxiosApiClient(TEST_URL)
+
+    nock(TEST_URL, {
+      reqheaders: {
+        "Content-Type": (headerValue) => headerValue === "application/json",
+        Authorization: (headerValue) => headerValue === "Bearer token",
+      },
+    })
+      .get("/posts/1")
+      .reply(200, { id: 1 })
+
+    const result = await client.get("/posts/1", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer token",
+      },
+    })
+    expect(result.status).toBe(200)
+    expect(result.data).toBeDefined()
+    expect(result.data).toHaveProperty("id")
+  })
+
+  it("setHeader method should set headers on axios instance", async () => {
+    const client = new AxiosApiClient(TEST_URL)
+
+    nock(TEST_URL, {
+      reqheaders: {
+        "Content-Type": (headerValue) => headerValue === "application/json",
+        Authorization: (headerValue) => headerValue === "Bearer token",
+      },
+    })
+      .get("/posts/1")
+      .reply(200, { id: 1 })
+
+    client.setHeader("Content-Type", "application/json")
+    client.setHeader("Authorization", "Bearer token")
+
+    const result = await client.get("/posts/1")
+    expect(result.status).toBe(200)
+    expect(result.data).toBeDefined()
+    expect(result.data).toHaveProperty("id")
+  })
+
+  it("setHeader method should override default headers set", async () => {
+    const client = new AxiosApiClient(TEST_URL, {
+      "Content-Type": "application/json",
+      Authorization: "Bearer token",
+    })
+
+    nock(TEST_URL, {
+      reqheaders: {
+        "Content-Type": (headerValue) => headerValue === "application/xml",
+        Authorization: (headerValue) => headerValue === "Bearer token",
+      },
+    })
+      .get("/posts/1")
+      .reply(200, { id: 1 })
+
+    client.setHeader("Content-Type", "application/xml")
+
+    const result = await client.get("/posts/1")
+    expect(result.status).toBe(200)
+    expect(result.data).toBeDefined()
+    expect(result.data).toHaveProperty("id")
+  })
+
+  it("request level headers should override default headers set", async () => {
+    const client = new AxiosApiClient(TEST_URL, {
+      "Content-Type": "application/json",
+    })
+
+    nock(TEST_URL, {
+      reqheaders: {
+        "Content-Type": (headerValue) => headerValue === "application/xml",
+      },
+    })
+      .get("/posts/1")
+      .reply(200, { id: 1 })
+
+    const result = await client.get("/posts/1", {
+      headers: {
+        "Content-Type": "application/xml",
+      },
+    })
+    expect(result.status).toBe(200)
+    expect(result.data).toBeDefined()
+    expect(result.data).toHaveProperty("id")
   })
 })
